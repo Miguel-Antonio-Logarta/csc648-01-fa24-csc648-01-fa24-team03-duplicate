f8b93ef287344d6beccdb36185ca7115ebea4cdf
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-10-11 20:13:59 -0700

    backend fully migrated from prototype, need to implement more PATCH and DELETE

---
+++ application/src/app/api/bookmarks/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../../../../prisma/prisma";
+
+/**
+ * @Endpoint - GET /api/bookmarks
+ * @description - Fetches all bookmarks from the database.
+ * @returns - all bookmarks in the database.
+ */
+export async function GET() {
+    try {
+        const bookmarks = await prisma.bookmark.findMany({
+            select: {
+                id: true,
+                user: {
+                    select: {
+                        id: true,
+                        username: true,
+                        email: true
+                    }
+                },
+                createdAt: true
+            }
+        });
+
+        if (!bookmarks || bookmarks.length === 0) {
+            return NextResponse.json({ error: 'No bookmarks found.' }, { status: 200 });
+        }
+
+        return NextResponse.json(bookmarks);
+    } catch (error: any) {
+        console.log(`[ERROR]: Error in GET of api/bookmarks/route.ts: ${error}`);
+        return NextResponse.json({ error: "Internal Server Error." }, { status: 500 });
+    }
+}
