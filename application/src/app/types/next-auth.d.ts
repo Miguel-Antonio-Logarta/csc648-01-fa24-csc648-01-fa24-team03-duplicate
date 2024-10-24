f50623fc4fd6e01896801603b35e15ec98beabdf
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-10-23 19:51:27 -0700

    nextauth implementation

---
+++ application/src/app/types/next-auth.d.ts
@@ -0,0 +1,27 @@
+import NextAuth, { DefaultSession, DefaultUser } from 'next-auth';
+
+// define the shape of the session object
+declare module 'next-auth' {
+  interface Session {
+    user: { // extend the user object with additional fields
+      id: string;
+      username: string;
+      email: string;
+      role: 'ADMIN' | 'CUSTOMER' | 'BUSINESS_OWNER' | 'MODERATOR';
+    } & DefaultSession['CUSTOMER'];
+  }
+
+  interface User extends DefaultUser {
+    // extend the user object with additional fields
+    username: string;
+    role: 'ADMIN' | 'CUSTOMER' | 'BUSINESS_OWNER' | 'MODERATOR';
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    // extend the JWT object with additional fields
+    username: string;
+    role: 'ADMIN' | 'CUSTOMER' | 'BUSINESS_OWNER' | 'MODERATOR';
+  }
+}
