6a6d42edd864641ea8e1fdade59549e63838db46
Miguel Logarta <maclogarta@gmail.com>
2024-10-26 01:27:37 -0700

    Navigation search bar

--- application/src/app/components/Navbar.tsx
+++ application/src/app/components/Navbar.tsx
@@ -1,38 +1,44 @@
-import Image from "next/image";
-import MagnifyingGlass from "./icons/MagnifyingGlass";
-import UserCircle from "./icons/UserCircle";
+"use client"
 
-// When the user is not logged, it should show login and sign up buttons
-// When the user is logged in, it should show the user's profile picture (clickable)
-// If the user does not have a profile picture, use the default profile picture.
+import Image from "next/image";
+import UserCircle from "./icons/UserCircle";
+import { Suspense, useState } from "react";
+import Link from "next/link";
+import NavSearch from "./NavSearch";
 
 const Navbar = () => {
+
+    const [loggedIn, setLoggedIn] = useState(false);
 
     return (
         <nav className="z-50 flex h-20 px-8 no-wrap items-stretch min-w-full shadow-md justify-between">
             <div className="flex items-stretch gap-x-4">
-                <Image src={"/logo/Catffeine.png"} alt="Catffeine. Our mascot." width={80} height={64} className="object-contain mr-4"/>
+                <Link href={"/"} passHref>
+                    <Image src={"/logo/Catffeine.png"} alt="Our mascot Catffeine" width={80} height={64} className="object-contain mr-4 h-auto"/>
+                </Link>
                 <div className="font-shantell flex items-stretch py-5">
-
-                    {/* Turn into a client component. Search bar makes a query */}
-                    <div className="flex items-stretch shadow-md bg-white px-6 rounded-full border-2 border-transparent hover:border-2 hover:border-slate-400">
-                        <input className="min-w-0 max-w-96 outline-none" placeholder="Cafes, libraries, parks..." />
-                        <MagnifyingGlass className="my-auto ml-4" size={20} />
-                    </div>
+                    <Suspense fallback={<p>Searchbar is loading...</p>}>
+                        <NavSearch />
+                    </Suspense>
                 </div>
             </div>
 
             <div className="flex no-wrap items-stretch gap-x-4">
                 <ul className="font-josefin flex no-wrap gap-x-4 mx-6">
-                    <li className="flex items-center capitalize">Help</li>
-                    <li className="flex items-center capitalize">About Us</li>
-                    <li className="flex items-center capitalize">Contact</li>
+                    <li><Link className="flex items-center capitalize w-full h-full align-middle hover:underline" href={"/help"}>Help</Link></li>
+                    <li><Link className="flex items-center capitalize w-full h-full align-middle hover:underline" href={"/about" }>About</Link></li>
+                    <li><Link className="flex items-center capitalize w-full h-full align-middle hover:underline" href={"/contact"}>Contact</Link></li>
                 </ul>
-
-                {/* Server component by default. However, if token is valid, or user is logged in, serve a client component that shoes the image or something */}
+                {loggedIn ?
                 <div className="flex no-wrap items-center">
                     <UserCircle size={32} />
+                </div> :
+                <div className="flex no-wrap items-center gap-5">
+                    <Link href={"/login"} className="bg-[#D1DAAF] border-[#D1DAAF] border-2 font-josefin px-4 py-2 shadow-md rounded-full hover:bg-white">Log in</Link>
+                    <Link href={"/signup"} className="bg-[#C6E2FF] border-[#C6E2FF] border-2 font-josefin px-4 py-2 shadow-md rounded-full hover:bg-white">Sign up</Link>
                 </div>
+                }
+                
             </div>
         </nav>
     );
