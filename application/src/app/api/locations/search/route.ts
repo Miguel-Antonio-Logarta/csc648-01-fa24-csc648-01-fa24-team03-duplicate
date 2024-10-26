1e308eceabe591b3fd17f61e8c826b9a66de07c6
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-10-26 13:02:32 -0700

    wrapped search with trycatch

--- application/src/app/api/locations/search/route.ts
+++ application/src/app/api/locations/search/route.ts
@@ -35,34 +35,38 @@ export interface OperatingHour {
  * @returns - a list of locations that match the search criteria
  */
 export async function GET(req: NextRequest) {
+    try {
+        const { searchParams } = new URL(req.nextUrl);
+        const name = searchParams.get('name');
+        const category = searchParams.get('category');
+        const hasWifi = searchParams.get('hasWifi');
+        const busynessStatus = searchParams.get('busynessStatus');
+        const radius = searchParams.get('radius');
 
-    const { searchParams } = new URL(req.nextUrl);
-    const name = searchParams.get('name');
-    const category = searchParams.get('category');
-    const hasWifi = searchParams.get('hasWifi');
-    const busynessStatus = searchParams.get('busynessStatus');
-    const radius = searchParams.get('radius');
+        // get all locations that match the search criteria
+        const filter: {
+            name?: { contains: string, mode: 'insensitive' },
+            category?: LocationType,
+            hasWifi?: boolean,
+            busynessStatus?: number,
+            radius?: number
+        } = {};
 
-    // get all locations that match the search criteria
-    const filter: {
-        name?: { contains: string, mode: 'insensitive' },
-        category?: LocationType,
-        hasWifi?: boolean,
-        busynessStatus?: number,
-        radius?: number
-    } = {};
+        if (name) filter.name = { contains: name, mode: 'insensitive' };
+        if (category) filter.category = category as LocationType;
+        if (hasWifi) filter.hasWifi = hasWifi === 'true';
+        if (busynessStatus) filter.busynessStatus = parseInt(busynessStatus);
+        if (radius) filter.radius = parseInt(radius);
 
-    if(name) filter.name = { contains: name, mode: 'insensitive' };
-    if(category) filter.category = category as LocationType;
-    if(hasWifi) filter.hasWifi = hasWifi === 'true';
-    if(busynessStatus) filter.busynessStatus = parseInt(busynessStatus);
-    if(radius) filter.radius = parseInt(radius);
+        const locations = await prisma.location.findMany({
+            where: filter,
+            include: { operatingHours: true }
+        });
 
-    const locations = await prisma.location.findMany({
-        where: filter,
-        include: { operatingHours: true }
-    });
-
-    // return the locations
-    return NextResponse.json(locations);
+        // return the locations
+        return NextResponse.json(locations);
+    } catch (error: any) {
+        console.log("[ERROR]: Error in GET of api/locations/search/route.ts: ", error);
+        return NextResponse.json({ error: "Internal Server Error." }, { status: 500 });
+    }
 }
