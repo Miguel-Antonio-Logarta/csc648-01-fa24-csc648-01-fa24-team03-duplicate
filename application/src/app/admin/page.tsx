a965b7cdbc4cbc0c5895bc68ba284d37537bfd8a
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-10-29 10:44:32 -0700

    fixed typo in filename, added unauthorized component

--- application/src/app/admin/page.tsx
+++ application/src/app/admin/page.tsx
@@ -1,24 +1,25 @@
 'use client';
 import { useSession } from "next-auth/react";
 import Link from 'next/link';
-import stars from '../about/timmytram/images/stars.png';
+import Unauthorized from "../components/Unauthorized";
 
 
 const Page = () => {
 
     const { data: session, status } = useSession();
 
+    // show nothing if loading for now
+    if(status === 'loading') return;
+
+    // show Unauthorized if not authenticated or not an admin
     if (status === 'unauthenticated' || session?.user.role !== 'ADMIN') {
-        return <div>Unauthorized</div>
+        return <Unauthorized />;
     }
 
+    // show the page if authenticated and an admin
     return (
         <div className="flex items-center justify-center min-h-screen text-black">
             <div className="relative bg-white p-8 rounded-lg shadow-lg text-center max-w-md overflow-hidden">
-
-                
-
-                {/* Content */}
                 <div className="relative z-10">
                     <h1 className="text-2xl font-bold mb-4">Welcome Admin: {session?.user.username}</h1>
                     <Link href="/admin/createLocation" className="block text-blue-300 hover:underline mt-2">
--- application/src/app/components/AdminNavbar.tsx
+++ application/src/app/components/AdminNavbar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import LogoutButton from "./LogoutButtont";
+import LogoutButton from "./LogoutButton";
 
 const AdminNavbar = () => {
 
