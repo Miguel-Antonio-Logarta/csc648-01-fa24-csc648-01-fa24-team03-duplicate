60d8af816ef1a37c07fd833fcf0c7d8c6a58aaf6
s000p <fumibunni@gmail.com>
2024-11-02 19:40:09 -0700

    profile

--- application/src/app/profile/BookmarksBox.tsx
+++ application/src/app/profile/BookmarksBox.tsx
@@ -1,6 +1,47 @@
-// components/BookmarksBox.tsx
 import React from 'react';
 import styles from './BookmarksBox.module.css';
+import Listing from "../search/Listing"
+
+
+
+const testData = [
+  {
+    id: 1,
+    img: "",
+    title: "J. Paul Leonard Library",
+    category: "University Library",
+    rating: 4.4,
+    wifiEnabled: true,
+    busyness: 3
+  },
+  {
+    id: 2,
+    img: "",
+    title: "Ocean View Branch Library",
+    category: "University Library",
+    rating: 4.0,
+    wifiEnabled: true,
+    busyness: 2
+  },
+  {
+    id: 3,
+    img: "",
+    title: "Marigold Cafe",
+    category: "Cafe",
+    rating: 4.5,
+    wifiEnabled: true,
+    busyness: 2
+  },
+  {
+    id: 4,
+    img: "",
+    title: "Home Coffee Roasters",
+    category: "Cafe",
+    rating: 4.0,
+    wifiEnabled: true,
+    busyness: 2
+  }
+]
 
 //application\src\app\profile\bookmarks.modules.css
 
@@ -8,8 +49,20 @@ const BookmarksBox: React.FC = () => {
   return (
     <div className={styles.bookmarksBox}>
       <h2 className={styles.title}>Bookmarks</h2>
-      {/* Placeholder for bookmarks */}
-      <div className={styles.bookmarkPlaceholder}>No bookmarks yet</div>
+      <div className="flex flex-col gap-6 mt-6">
+          {testData.map((data) => 
+            <Listing 
+              key={data.id}
+              img={data.img}
+              title={data.title}
+              category={data.category}
+              rating={data.rating}
+              wifiEnabled={data.wifiEnabled}
+              busyness={data.busyness}
+              />)
+          }
+        </div>
+    {/* /* <div className={styles.bookmarkPlaceholder}>No bookmarks yet</div>*/ }
     </div>
   );
 };
--- application/src/app/profile/bio.tsx
+++ application/src/app/profile/bio.tsx
@@ -1,13 +1,15 @@
-// components/ProfileCard.tsx
 import React from 'react';
 import styles from './bio.module.css';
 
 const bio: React.FC = () => {
   return (
     <div className={styles.card}>
-      <div className={styles.avatarPlaceholder}></div>
-      <h2 className={styles.username}>Catffeine648</h2>
-      <button className={styles.editButton}>Edit Profile</button>
+      <div className={styles.avatarPlaceholder}></div> 
+      <h2 className={styles.username}>Catffeine648</h2> 
+      <button className={styles.editButton}>Edit Profile</button> 
+      <p className={styles.socialNumbers}>X followers | X Following</p>
+      <p>user bio here</p>
+
     </div>
   );
 };
