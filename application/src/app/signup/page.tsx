fdff535ecef192fe2a5fc76e81d62af67d029e03
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-11-02 20:40:03 -0700

    sign up functional

--- application/src/app/signup/page.tsx
+++ application/src/app/signup/page.tsx
@@ -1,4 +1,9 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import React from 'react';
+import useCreateUser from "../hooks/useCreateUser";
 
 const Header: React.FC = () => {
   return (
@@ -9,21 +14,59 @@ const Header: React.FC = () => {
 };
 
 const SignUpForm: React.FC = () => {
+  const [userInfo, setUserInfo] = useState({ username: "", email: "", password: "" });
+  const router = useRouter();
+  const { createUser, loading } = useCreateUser();
+  
+  
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await createUser(userInfo);
+    router.push('/login');
+  }
+
   return (
     <div className="flex flex-col ml-5 w-[45%] max-md:ml-0 max-md:w-full">
-      <form className="flex flex-col gap-1.5 items-start self-stretch my-auto -ml-px text-stone-600 max-md:mt-10">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-1.5 items-start self-stretch my-auto -ml-px text-stone-600 max-md:mt-10">
         <h2 className="text-2xl font-bold tracking-[2.25px]">Sign Up</h2>
         <div className="mt-3 text-xs text-stone-400">
-          <label htmlFor="name" className="sr-only">Name</label>
-          <input type="text" id="name" className="w-full bg-transparent border-b-2 border-rose-400 border-dashed" aria-label="Name" />
+          <label htmlFor="username" className="sr-only">Username</label>
+          <input 
+          type="text" 
+          id="username" 
+          value={userInfo.username}
+          onChange={(e) => setUserInfo({ ...userInfo, username: e.target.value })}
+          className="w-full bg-transparent border-b-2 border-rose-400 border-dashed" 
+          aria-label="username"
+          autoComplete="off"
+          required
+           />
         </div>
         <div className="mt-3 text-xs text-stone-400">
-          <label htmlFor="password" className="sr-only">Email</label>
-          <input type="email" id="email" className="w-full bg-transparent border-b-2 border-rose-400 border-dashed" aria-label="Password" />
+          <label htmlFor="email" className="sr-only">Email</label>
+          <input 
+          type="email" 
+          id="email"
+          value={userInfo.email}
+          onChange={(e) => setUserInfo({ ...userInfo, email: e.target.value })} 
+          className="w-full bg-transparent border-b-2 border-rose-400 border-dashed" 
+          aria-label="Password"
+          autoComplete="off"
+          required
+           />
         </div>
         <div className="mt-3 text-xs text-stone-400">
           <label htmlFor="password" className="sr-only">Password</label>
-          <input type="password" id="password" className="w-full bg-transparent border-b-2 border-rose-400 border-dashed" aria-label="Password" />
+          <input 
+          type="password" 
+          id="password"
+          value={userInfo.password}
+          onChange={(e) => setUserInfo({ ...userInfo, password: e.target.value })} 
+          className="w-full bg-transparent border-b-2 border-rose-400 border-dashed" 
+          aria-label="Password"
+          autoComplete="off"
+          required
+           />
         </div>
         <button type="submit" className="self-center px-4 py-1.5 mt-10 text-sm font-bold text-center text-white drop-shadow-mdç bg-sage rounded-[35px] w-[86px] max-md:mt-10">
           Sign Up!
