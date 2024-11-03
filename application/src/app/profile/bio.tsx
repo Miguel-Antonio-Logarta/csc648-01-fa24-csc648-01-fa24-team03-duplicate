36ddf783386645a0849285cf8c1676c0ff114bf5
s000p <fumibunni@gmail.com>
2024-11-02 20:27:33 -0700

    changed size of something

--- application/src/app/profile/bio.tsx
+++ application/src/app/profile/bio.tsx
@@ -8,7 +8,7 @@ const bio: React.FC = () => {
       <h2 className={styles.username}>Catffeine648</h2> 
       <button className={styles.editButton}>Edit Profile</button> 
       <p className={styles.socialNumbers}>X followers | X Following</p>
-      <p>user bio here</p>
+      <p>user bio here aougaeiphpaei paiegh paiehgpiaeh gpiae hgpiaeh gpia hgp isghaeip haeih pisgh pis aehihipfah ipfh aeid hpiah fpaegh pihg pifgh psfgh p i</p>
 
     </div>
   );
