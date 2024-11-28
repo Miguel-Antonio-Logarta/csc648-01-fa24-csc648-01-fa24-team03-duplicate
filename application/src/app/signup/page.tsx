00431b3efa13cc4c81f5983f1d39481ff45cb78c
John Bagwell <jbagwell@mail.sfsu.edu>
2024-11-27 17:38:11 -0800

    Fixed signup page

--- application/src/app/signup/page.tsx
+++ application/src/app/signup/page.tsx
@@ -1,11 +1,14 @@
-'use client';
+"use client"
 import React from 'react';
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { useSession } from 'next-auth/react';
+import { signIn } from "next-auth/react";
+import { useState, useRef } from "react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+import Image from 'next/image';
+import signupCatTrimmed from "./signup_cat_trimmed.png";
 import useCreateUser from '../hooks/useCreateUser';
 
-// TODO: Move into /components. Only one component per file.
+
 const SignUpForm: React.FC = () => {
   const [userInfo, setUserInfo] = useState({ username: "", email: "", password: "" });
   const { createUser, loading } = useCreateUser();
@@ -17,61 +20,88 @@ const SignUpForm: React.FC = () => {
     router.push('/login');
   };
 
+  
 
   return (
-    <div className="flex flex-col ml-5 w-[45%] max-md:ml-0 max-md:w-full">
-      <form onSubmit={handleSubmit} className="flex flex-col gap-1.5 items-start self-stretch my-auto -ml-px text-stone-600 max-md:mt-10">
-        <h2 className="text-2xl font-bold tracking-[2.25px]">Sign Up</h2>
-        <div className="mt-3 text-xs text-stone-400">
-          <label htmlFor="usernname" className="sr-only">Username</label>
-          <input
-            type="text"
-            id="username"
-            className="w-full bg-transparent border-b-2 border-rose-400 border-dashed"
-            value={userInfo.username}
-            onChange={(e) => setUserInfo({ ...userInfo, username: e.target.value })}
-            aria-label="Name"
-            autoComplete="off"
-            placeholder='Username'
-            required />
-        </div>
-        <div className="mt-3 text-xs text-stone-400">
-          <label htmlFor="password" className="sr-only">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={userInfo.email}
-            onChange={(e) => setUserInfo({ ...userInfo, email: e.target.value })}
-            className="w-full bg-transparent border-b-2 border-rose-400 border-dashed"
-            aria-label="Password"
-            autoComplete="off"
-            placeholder='Email'
-          />
-        </div>
-        <div className="mt-3 text-xs text-stone-400">
-          <label htmlFor="password" className="sr-only">Password</label>
-          <input 
-          type="password" 
-          id="password"
-          value={userInfo.password}
-          onChange={(e) => setUserInfo({ ...userInfo, password: e.target.value })} 
-          className="w-full bg-transparent border-b-2 border-rose-400 border-dashed" 
-          aria-label="Password" 
-          autoComplete="off" 
-          placeholder='Password'
-          required 
-          />
-        </div>
-        <button type="submit" className="self-center px-4 py-1.5 mt-10 text-sm font-bold text-center text-white drop-shadow-mdç bg-sage rounded-[35px] max-md:mt-10 hover:bg-white hover:text-sage border-[#D1DAAF] border-2">
-          Sign Up!
-        </button>
-      </form>
+    <div className="grid place-items-center h-screen fixed inset-x-0 bottom-16 bg-opacity-50 items-center justify-center">
+      <main className="mx-auto max-w-[594px] max-h-[415px] bg-transparent ">
+        <section className=" pb-14 px-10 mx-auto w-full bg-yellow-50 border-4 border-rose-400 border-solid rounded-[35px] shadow-[0px_3px_3px_rgba(0,0,0,0.25)]">
+          <h1 className="font-shantell z-10 mx-auto px-16 pt-5 pb-5 mt-[-2rem] mb-1 max-w-full text-4xl font-extrabold text-center text-rose-500 bg-rose-300 rounded-2xl border-rose-400 border-dashed border-[3px] tracking-[3.38px] w-[370px] max-md:px-5">
+            Sign up!
+          </h1>
+          <div className="grid grid-cols-8 mt-1.5 max-md:max-w-full">
+          <figure className="col-span-3 items-center max-md:ml-0 max-md:w-full">
+              <Image
+                src={signupCatTrimmed}
+                className="object-contain w-[120%] h-[120%] max-md:w-full max-md:h-full"
+                alt={'Login Cat Logo'}
+              />
+            </figure>
+            <div className="col-span-5 flex ml-5 max-md:ml-0 max-md:w-full">
+              <form onSubmit={handleSubmit} className="flex flex-col items-start -mr-0.5 text-xs text-stone-400">
+                <h2 className="text-2xl font-bold text-stone-600 tracking-[2.25px]">
+                  Enter info
+                </h2>
+                <div className="mt-3 text-xs text-stone-400">
+                  <label htmlFor="login" className="sr-only">Username</label>
+                  <input
+                    type="text"
+                    id="username"
+                    value={userInfo.username}
+                    onChange={(e) => setUserInfo({ ...userInfo, username: e.target.value })}
+                    placeholder='Username'
+                    className={`shrink-0 text-black mt-1 pl-2 pr-14 py-3 h-10 text-lg bg-yellow-50 border-b-2 border-dashed w-full w-[18rem] hover:bg-[#fef9c3] }`}
+                    aria-label="username"
+                    autoComplete="off"
+                    required
+                  />
+                </div>
+                <div className="mt-3 text-xs text-stone-400">
+                  <label htmlFor="password" className="sr-only">Password</label>
+                  <input
+                    type="email"
+                    id="email"
+                    value={userInfo.email}
+                    onChange={(e) => setUserInfo({ ...userInfo, email: e.target.value })}
+                    className={`shrink-0 text-black self-stretch mt-1 pl-2 pr-14 py-3 h-10 text-lg bg-yellow-50 border-b-2 border-dashed w-full w-[18rem] hover:bg-[#fef9c3] }`}
+                    placeholder='Email'
+                    aria-label="Email"
+                    autoComplete="off"
+                    required
+                  />
+                </div>
+                <div className="mt-3 text-xs text-stone-400">
+                  <label htmlFor="password" className="sr-only">Password</label>
+                  <input
+                    type="password"
+                    id="password"
+                    value={userInfo.password}
+                    onChange={(e) => setUserInfo({ ...userInfo, password: e.target.value })}
+                    className={`shrink-0 text-black self-stretch mt-1 pl-2 pr-14 py-3 h-10 text-lg bg-yellow-50 border-b-2 border-dashed w-full w-[18rem] hover:bg-[#fef9c3] }`}
+                    placeholder='Password'
+                    aria-label="Password"
+                    autoComplete="off"
+                    required
+                  />
+                </div>
+                <div className="relative w-full">
+                </div>
+                <button
+                  type="submit"
+                  className="font-shantell self-center px-3.5 py-1.5  mt-10 text-lg font-bold text-center text-white bg-sage shadow-md rounded-[35px] w-[136px] hover:bg-white hover:text-sage border-[#D1DAAF] border-2"
+                >
+                  Submit!
+                </button>
+              </form>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
-};
+}
 
-const Page = () => {
-
+const Page: React.FC = () => {
   const { data: session, status } = useSession();
   const router = useRouter();
 
@@ -81,24 +111,8 @@ const Page = () => {
   }
 
   return (
-    <main className="flex flex-col max-w-[594px] mx-auto mt-larger">
-      <section className="flex overflow-hidden flex-col pr-6 pb-16 pl-6 w-full bg-yellow-50 border-4 border-rose-400 border-solid shadow-sm rounded-[35px] max-md:px-5 max-md:max-w-full">
-        {/* <Header /> */}
-        <div className="self-stretch mt-1.5 max-md:max-w-full">
-          <div className="flex gap-5 max-md:flex-col">
-            <div className="flex flex-col w-[55%] max-md:ml-0 max-md:w-full">
-              {/* TODO: Change into Next.js <Image /> Component */}
-              <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/d0540ee4e9866f761379c86adda960de2ef26a725a46efeb28976bd1c2e7fecf?placeholderIfAbsent=true&apiKey=dae5425d3b3c4cdc84ccb32ea9568225" alt="Login illustration" className="object-contain grow w-full aspect-[1.03] max-w-[279px]" />
-            </div>
-            <SignUpForm />
-          </div>
-        </div>
-      </section>
-    </main>
+          <SignUpForm />
   );
-}
+};
 
-// const SignupPage: React.FC = () => {
-
-// }
-export default Page
+export default Page;
