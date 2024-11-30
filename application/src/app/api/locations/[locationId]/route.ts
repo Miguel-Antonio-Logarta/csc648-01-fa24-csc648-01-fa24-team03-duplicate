c69943be49a4de6b8cc9bf87243e63da15d54817
Timmy Tram <74998146+TimmyTram@users.noreply.github.com>
2024-11-30 09:19:58 -0800

    api endpoint for edit locations

--- application/src/app/api/locations/[locationId]/route.ts
+++ application/src/app/api/locations/[locationId]/route.ts
@@ -81,12 +81,11 @@ export async function DELETE(req: NextRequest, { params } : { params: { location
         }
         
         // this only exists for now because we have items in the db that don't have an image
-        if(location.imageWebLink && location.imageWebLink.length !== 0 && location.imageWebLink !== "N/A") {
-            await del(location.imageWebLink as string);
-        } 
+        // if(location.imageWebLink && location.imageWebLink.length !== 0 && location.imageWebLink !== "N/A") {
+        //     await del(location.imageWebLink as string);
+        // } 
 
-        // uncomment this when all locations have an image
-        //await del(location.imageWebLink as string);
+        await del(location.imageWebLink as string);
 
         const deletedLocation = await prisma.location.delete({
             where: {
@@ -99,4 +98,117 @@ export async function DELETE(req: NextRequest, { params } : { params: { location
         console.log(`[ERROR]: Error in DELETE of api/locations/[locationId]/route.ts: ${err}`);
         return NextResponse.json({ error: "Internal Server Error." }, { status: 500 });
     }
+}
+
+/**
+ * @Auth - Required to be ADMIN
+ * @Endpoint - PATCH /api/locations/{locationId}
+ * @description - Updates a single location from the database given a location id.
+ * @returns - The updated location.
+ */
+export async function PATCH(req: NextRequest, { params } : { params: { locationId : string}}) {
+
+    const session = await getServerSession(authOptions);
+
+    if(!session || session.user.role !== 'ADMIN') {
+        return NextResponse.json({ error: "Unauthorized." }, { status: 401 });
+    }
+
+    try {
+        const locationId = params.locationId;
+
+        if(!locationId) {
+            return NextResponse.json({ error: "No locationId provided." }, { status: 400 });
+        }
+
+        const location = await prisma.location.findFirst({
+            where: {
+                id: locationId
+            }
+        });
+
+        if(!location) {
+            return NextResponse.json({ error: "Location not found." }, { status: 404 });
+        }
+
+        // all allowed fields that can be updated.
+        const { 
+            name, 
+            address, 
+            phoneNumber, 
+            hasWifi, 
+            seatingCapacity, 
+            category, 
+            locationWebsiteLink, 
+            animalFriendliness, 
+            latitude, 
+            longitude, 
+            operatingHours 
+        } = await req.json();
+
+        // Check if any of the fields are empty.
+        if(!name && !address && !phoneNumber && !hasWifi && !seatingCapacity && !category && !locationWebsiteLink && !animalFriendliness && !latitude && !longitude && !operatingHours) {
+            return NextResponse.json({ error: "No fields provided to update." }, { status: 400 });
+        }
+
+        // ensure that address is unique
+        if(address) {
+            const existingLocation = await prisma.location.findFirst({
+                where: {
+                    address: address
+                }
+            });
+
+            if(existingLocation && existingLocation.id !== locationId) {
+                return NextResponse.json({ error: "Location with this address already exists." }, { status: 400 });
+            }
+        }
+
+        // Update the location with the new fields.
+        const updatedLocation = await prisma.location.update({
+            where: {
+                id: locationId
+            },
+            data: {
+                name: name ? name : location.name,
+                address: address ? address : location.address,
+                phoneNumber: phoneNumber ? phoneNumber : location.phoneNumber,
+                hasWifi: hasWifi ? hasWifi : location.hasWifi,
+                seatingCapacity: seatingCapacity ? seatingCapacity : location.seatingCapacity,
+                category: category ? category : location.category,
+                locationWebsiteLink: locationWebsiteLink ? locationWebsiteLink : location.locationWebsiteLink,
+                animalFriendliness: animalFriendliness ? animalFriendliness : location.animalFriendliness,
+                latitude: latitude ? latitude : location.latitude,
+                longitude: longitude ? longitude : location.longitude,
+            }
+        });
+
+        // Update the operating hours if they are provided.
+        if(operatingHours) {
+            // delete all operating hours for the location
+            await prisma.operatingHours.deleteMany({
+                where: {
+                    locationId: locationId
+                }
+            });
+
+            // create new operating hours for the location
+            for (const operatingHour of operatingHours) {
+                await prisma.operatingHours.create({
+                    data: {
+                        day: operatingHour.day,
+                        openTime: operatingHour.openTime,
+                        closeTime: operatingHour.closeTime,
+                        locationId: locationId
+                    }
+                });
+            }
+        }
+
+        return NextResponse.json(updatedLocation);
+    } catch (err : any) {
+        console.log(`[ERROR]: Error in PATCH of api/locations/[locationId]/route.ts: ${err}`);
+        return NextResponse.json({ error: "Internal Server Error." }, { status: 500 });
+    }
+
 }
