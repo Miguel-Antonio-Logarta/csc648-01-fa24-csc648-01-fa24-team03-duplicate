d3aa3b9a1377a55e00ebedd1b833c86ff5071cb5
John Bagwell <jbagwell@mail.sfsu.edu>
2024-12-09 06:42:51 -0800

    Added tactility to logo in navbar (optional)

--- application/src/app/components/Navbar.tsx
+++ application/src/app/components/Navbar.tsx
@@ -1,25 +1,39 @@
+"use client";
+
 import Image from 'next/image';
 import Link from 'next/link';
 import NavSearch from './NavSearch';
 import NavbarUser from './NavbarUser';
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 
 const Navbar = () => {
+  const [imageSrc, setImageSrc] = useState('/logo/Catffeine.png');
+
+  const handleMouseEnter = () => {
+    setImageSrc('/logo/Catffeine2.png');
+  };
+
+  const handleMouseLeave = () => {
+    setImageSrc('/logo/Catffeine.png');
+  };
+
   return (
     <nav className="z-50 flex h-20 px-8 no-wrap items-stretch min-w-full shadow-md justify-between">
       <div className="flex items-stretch gap-x-4">
         <Link href={'/'} passHref>
           <Image
-            src={'/logo/Catffeine.png'}
+            src={imageSrc}
             alt="Our mascot Catffeine"
             width={80}
             height={64}
             className="object-contain mr-4 h-auto"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
           />
         </Link>
         <div className="font-shantell flex items-stretch py-5">
           <Suspense fallback={<div>Searchbar is loading...</div>}>
-              <NavSearch />
+            <NavSearch />
           </Suspense>
         </div>
       </div>
